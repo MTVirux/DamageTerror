c352fd3a11a7eec1965cff68ac429c6f7f0340a4
MTVirux <joaofraga96@outlook.com>
2026-04-05 18:52:53 +0100

    Add MetricPicker: centralized metric label and picker UI component
    Introduces a reusable generic MetricPicker class that consolidates:
    - BarColumn and TooltipField label dictionaries
    - Category definitions for both metric types
    - Draw() for ordered enable/disable lists with reorder arrows
    - DrawCategorized() for tabbed category-based metric selection

---
+++ DamageTerror/Gui/ConfigWindow/MetricPicker.cs
@@ -0,0 +1,350 @@
+using Dalamud.Bindings.ImGui;
+using DamageTerror.Enums;
+using ImGui = Dalamud.Bindings.ImGui.ImGui;
+
+namespace DamageTerror.Gui.ConfigWindow;
+
+public static class MetricPicker
+{
+    public static readonly (string Name, BarColumn[] Items)[] BarColumnCategories =
+    {
+        ("Damage", new[] { BarColumn.Dps, BarColumn.InstantDps, BarColumn.PeakDps, BarColumn.Damage, BarColumn.DamagePercent, BarColumn.MaxHit, BarColumn.MaxHitValue, BarColumn.DamageShield, BarColumn.RaidDps }),
+        ("Healing", new[] { BarColumn.Hps, BarColumn.InstantHps, BarColumn.Healed, BarColumn.HealPercent, BarColumn.Overheal, BarColumn.OverhealAmount, BarColumn.CritHealPct, BarColumn.MaxHeal, BarColumn.MaxHealValue, BarColumn.HealCount, BarColumn.RaidHps }),
+        ("Hit Stats", new[] { BarColumn.Crit, BarColumn.DirectHit, BarColumn.CritDirectHit, BarColumn.CritHitCount, BarColumn.DirectHitCount, BarColumn.CritDirectHitCount, BarColumn.HitRate, BarColumn.Swings, BarColumn.Hits, BarColumn.Misses }),
+        ("Defense", new[] { BarColumn.DamageTaken, BarColumn.DamageTakenPercent, BarColumn.BlockPct, BarColumn.ParryPct, BarColumn.HealsTaken }),
+        ("Other", new[] { BarColumn.Deaths, BarColumn.Kills, BarColumn.CombatantDuration, BarColumn.PowerHeal }),
+        ("Brainrot", new[] { BarColumn.Stuns, BarColumn.SkillIssue }),
+#if DEBUG
+        ("Unknown", new[] { BarColumn.PowerDrain, BarColumn.AbsorbHeal, BarColumn.MaxHealWard }),
+#endif
+    };
+
+    public static readonly (string Name, TooltipField[] Items)[] TooltipFieldCategories =
+    {
+        ("Damage", new[] { TooltipField.Dps, TooltipField.InstantDps, TooltipField.PeakDps, TooltipField.Damage, TooltipField.DamagePercent, TooltipField.MaxHit, TooltipField.MaxHitValue, TooltipField.DamageShield, TooltipField.RaidDps, TooltipField.TopDamageSkills }),
+        ("Healing", new[] { TooltipField.Hps, TooltipField.InstantHps, TooltipField.Healed, TooltipField.HealPercent, TooltipField.Overheal, TooltipField.OverhealAmount, TooltipField.CritHealPct, TooltipField.MaxHeal, TooltipField.MaxHealValue, TooltipField.MaxHealWard, TooltipField.HealCount, TooltipField.RaidHps, TooltipField.TopHealingSkills }),
+        ("Hit Stats", new[] { TooltipField.Crit, TooltipField.DirectHit, TooltipField.CritDirectHit, TooltipField.HitRate, TooltipField.Swings, TooltipField.Hits, TooltipField.Misses }),
+        ("Defense", new[] { TooltipField.DamageTaken, TooltipField.HealsTaken }),
+        ("Other", new[] { TooltipField.Name, TooltipField.Job, TooltipField.Deaths, TooltipField.Kills, TooltipField.CombatantDuration }),
+        ("Brainrot", new[] { TooltipField.Stuns, TooltipField.SkillIssue }),
+    };
+
+    public static readonly Dictionary<BarColumn, string> BarColumnLabels = new()
+    {
+        { BarColumn.Dps, "DPS" },
+        { BarColumn.Hps, "HPS" },
+        { BarColumn.Damage, "Damage" },
+        { BarColumn.Healed, "Healed" },
+        { BarColumn.DamagePercent, "Damage %" },
+        { BarColumn.HealPercent, "Heal %" },
+        { BarColumn.DirectHit, "Direct Hit %" },
+        { BarColumn.Crit, "Critical Hit %" },
+        { BarColumn.CritDirectHit, "Crit Direct Hit %" },
+        { BarColumn.Deaths, "Deaths" },
+        { BarColumn.DamageTaken, "Damage Taken" },
+        { BarColumn.DamageTakenPercent, "Damage Taken %" },
+        { BarColumn.Overheal, "Overheal %" },
+        { BarColumn.OverhealAmount, "Overheal Amount" },
+        { BarColumn.MaxHit, "Highest Hit" },
+        { BarColumn.MaxHitValue, "Highest Hit Value" },
+        { BarColumn.PeakDps, "Peak DPS" },
+        { BarColumn.MaxHeal, "Max Heal" },
+        { BarColumn.MaxHealValue, "Max Heal Value" },
+        { BarColumn.Swings, "Swings" },
+        { BarColumn.Hits, "Hits" },
+        { BarColumn.Misses, "Misses" },
+        { BarColumn.HitRate, "Hit Rate" },
+        { BarColumn.CritHitCount, "Crit Hit Count" },
+        { BarColumn.DirectHitCount, "Direct Hit Count" },
+        { BarColumn.CritDirectHitCount, "Crit DH Count" },
+        { BarColumn.BlockPct, "Block %" },
+        { BarColumn.ParryPct, "Parry %" },
+        { BarColumn.HealsTaken, "Heals Taken" },
+        { BarColumn.AbsorbHeal, "Absorb Heal" },
+        { BarColumn.Kills, "Kills" },
+        { BarColumn.InstantDps, "Instant DPS" },
+        { BarColumn.InstantHps, "Instant HPS" },
+        { BarColumn.CritHealPct, "Crit Heal %" },
+        { BarColumn.HealCount, "Heal Count" },
+        { BarColumn.CombatantDuration, "Duration" },
+        { BarColumn.DamageShield, "Shield Damage" },
+        { BarColumn.MaxHealWard, "Max Heal Ward" },
+        { BarColumn.PowerDrain, "MP Drain" },
+        { BarColumn.PowerHeal, "MP Recovery" },
+        { BarColumn.Stuns, "Stuns" },
+        { BarColumn.SkillIssue, "Skill Issue" },
+        { BarColumn.RaidDps, "Group DPS" },
+        { BarColumn.RaidHps, "Group HPS" },
+    };
+
+    public static readonly Dictionary<TooltipField, string> TooltipFieldLabels = new()
+    {
+        { TooltipField.Name, "Name" },
+        { TooltipField.Job, "Job" },
+        { TooltipField.Dps, "DPS" },
+        { TooltipField.Hps, "HPS" },
+        { TooltipField.Damage, "Damage" },
+        { TooltipField.Healed, "Healed" },
+        { TooltipField.DamagePercent, "Damage %" },
+        { TooltipField.HealPercent, "Heal %" },
+        { TooltipField.Crit, "Crit %" },
+        { TooltipField.DirectHit, "Direct Hit %" },
+        { TooltipField.CritDirectHit, "Crit DH %" },
+        { TooltipField.Deaths, "Deaths" },
+        { TooltipField.DamageTaken, "Damage Taken" },
+        { TooltipField.Overheal, "Overheal %" },
+        { TooltipField.OverhealAmount, "Overheal" },
+        { TooltipField.MaxHit, "Max Hit" },
+        { TooltipField.MaxHitValue, "Max Hit Value" },
+        { TooltipField.MaxHeal, "Max Heal" },
+        { TooltipField.MaxHealValue, "Max Heal Value" },
+        { TooltipField.PeakDps, "Peak DPS" },
+        { TooltipField.Swings, "Swings" },
+        { TooltipField.Hits, "Hits" },
+        { TooltipField.Misses, "Misses" },
+        { TooltipField.HitRate, "Hit Rate" },
+        { TooltipField.Kills, "Kills" },
+        { TooltipField.CombatantDuration, "Duration" },
+        { TooltipField.HealsTaken, "Heals Taken" },
+        { TooltipField.InstantDps, "Instant DPS" },
+        { TooltipField.InstantHps, "Instant HPS" },
+        { TooltipField.CritHealPct, "Crit Heal %" },
+        { TooltipField.HealCount, "Heal Count" },
+        { TooltipField.DamageShield, "Damage Shield" },
+        { TooltipField.MaxHealWard, "Max Heal Ward" },
+        { TooltipField.Stuns, "Stuns" },
+        { TooltipField.SkillIssue, "Skill Issue" },
+        { TooltipField.RaidDps, "Group DPS" },
+        { TooltipField.RaidHps, "Group HPS" },
+        { TooltipField.TopDamageSkills, "Top Damage Skills" },
+        { TooltipField.TopHealingSkills, "Top Healing Skills" },
+    };
+
+    public static string GetBarColumnLabel(BarColumn col) =>
+        BarColumnLabels.GetValueOrDefault(col, col.ToString());
+
+    public static string GetTooltipFieldLabel(TooltipField field) =>
+        TooltipFieldLabels.GetValueOrDefault(field, field.ToString());
+
+    /// <summary>
+    /// Draws a metric picker with an ordered enabled list (with reorder arrows) at the top,
+    /// and disabled items organized in categorized tabs below.
+    /// </summary>
+    public static bool Draw<T>(
+        string id,
+        List<T> enabledItems,
+        Func<T, string> getLabel,
+        (string Name, T[] Items)[] categories,
+        Func<T, bool>? drawItemExtras = null) where T : struct, Enum
+    {
+        var changed = false;
+
+        var enabledSet = new HashSet<T>(enabledItems);
+
+        for (var i = 0; i < enabledItems.Count; i++)
+        {
+            var item = enabledItems[i];
+            var label = getLabel(item);
+
+            ImGui.PushID($"{id}_e_{i}");
+
+            var canUp = i > 0;
+            if (!canUp) ImGui.BeginDisabled();
+            if (ImGui.ArrowButton("##up", ImGuiDir.Up))
+            {
+                (enabledItems[i - 1], enabledItems[i]) = (enabledItems[i], enabledItems[i - 1]);
+                changed = true;
+            }
+            if (!canUp) ImGui.EndDisabled();
+
+            ImGui.SameLine();
+
+            var canDown = i < enabledItems.Count - 1;
+            if (!canDown) ImGui.BeginDisabled();
+            if (ImGui.ArrowButton("##down", ImGuiDir.Down))
+            {
+                (enabledItems[i], enabledItems[i + 1]) = (enabledItems[i + 1], enabledItems[i]);
+                changed = true;
+            }
+            if (!canDown) ImGui.EndDisabled();
+
+            ImGui.SameLine();
+
+            var enabled = true;
+            if (ImGui.Checkbox(label, ref enabled))
+            {
+                enabledItems.RemoveAt(i);
+                enabledSet.Remove(item);
+                i--;
+                changed = true;
+                ImGui.PopID();
+                continue;
+            }
+
+            if (drawItemExtras != null)
+                changed |= drawItemExtras(item);
+
+            ImGui.PopID();
+        }
+
+        ImGui.Spacing();
+        ImGui.Separator();
+        ImGui.TextDisabled("Disabled");
+        ImGui.Spacing();
+
+        var hasAnyDisabled = false;
+        foreach (var (_, catItems) in categories)
+            foreach (var item in catItems)
+                if (!enabledSet.Contains(item))
+                {
+                    hasAnyDisabled = true;
+                    break;
+                }
+
+        if (hasAnyDisabled && ImGui.BeginTabBar($"##{id}_disabled"))
+        {
+            foreach (var (catName, catItems) in categories)
+            {
+                var catDisabled = new List<T>();
+                foreach (var item in catItems)
+                    if (!enabledSet.Contains(item))
+                        catDisabled.Add(item);
+
+                if (catDisabled.Count == 0)
+                    continue;
+
+                if (ImGui.BeginTabItem(catName))
+                {
+                    catDisabled.Sort((a, b) =>
+                        string.Compare(getLabel(a), getLabel(b), StringComparison.OrdinalIgnoreCase));
+
+                    foreach (var item in catDisabled)
+                    {
+                        var label = getLabel(item);
+                        ImGui.PushID($"{id}_d_{item}");
+
+                        var off = false;
+                        if (ImGui.Checkbox(label, ref off))
+                        {
+                            enabledItems.Add(item);
+                            enabledSet.Add(item);
+                            changed = true;
+                        }
+
+                        ImGui.PopID();
+                    }
+
+                    ImGui.EndTabItem();
+                }
+            }
+
+            ImGui.EndTabBar();
+        }
+
+        return changed;
+    }
+
+    /// <summary>
+    /// Draws a categorized metric picker with tabs per category.
+    /// Each category tab shows items with optional reorder arrows and visibility checkboxes.
+    /// </summary>
+    public static bool DrawCategorized<T>(
+        string id,
+        HashSet<T> enabledSet,
+        Func<T, string> getLabel,
+        (string Name, T[] Items)[] categories,
+        Dictionary<string, List<T>>? sectionOrder = null) where T : struct, Enum
+    {
+        var changed = false;
+
+        if (!ImGui.BeginTabBar($"##{id}_cats"))
+            return changed;
+
+        foreach (var (catName, catItems) in categories)
+        {
+            if (!ImGui.BeginTabItem(catName))
+                continue;
+
+            List<T> order;
+            if (sectionOrder != null)
+            {
+                if (!sectionOrder.TryGetValue(catName, out order!) || order.Count == 0)
+                {
+                    order = new List<T>(catItems);
+                    sectionOrder[catName] = order;
+                }
+
+                foreach (var item in catItems)
+                    if (!order.Contains(item))
+                        order.Add(item);
+
+                var validItems = new HashSet<T>(catItems);
+                order.RemoveAll(item => !validItems.Contains(item));
+            }
+            else
+            {
+                order = new List<T>(catItems);
+            }
+
+            for (var i = 0; i < order.Count; i++)
+            {
+                var item = order[i];
+                var label = getLabel(item);
+                var enabled = enabledSet.Contains(item);
+
+                ImGui.PushID($"{id}_c_{catName}_{i}");
+
+                if (sectionOrder != null)
+                {
+                    var canUp = i > 0;
+                    if (!canUp) ImGui.BeginDisabled();
+                    if (ImGui.ArrowButton("##up", ImGuiDir.Up))
+                    {
+                        (order[i], order[i - 1]) = (order[i - 1], order[i]);
+                        changed = true;
+                    }
+                    if (!canUp) ImGui.EndDisabled();
+
+                    ImGui.SameLine();
+
+                    var canDown = i < order.Count - 1;
+                    if (!canDown) ImGui.BeginDisabled();
+                    if (ImGui.ArrowButton("##down", ImGuiDir.Down))
+                    {
+                        (order[i], order[i + 1]) = (order[i + 1], order[i]);
+                        changed = true;
+                    }
+                    if (!canDown) ImGui.EndDisabled();
+
+                    ImGui.SameLine();
+                }
+
+                if (ImGui.Checkbox(label, ref enabled))
+                {
+                    if (enabled)
+                        enabledSet.Add(item);
+                    else
+                        enabledSet.Remove(item);
+                    changed = true;
+                }
+
+                ImGui.PopID();
+            }
+
+            if (sectionOrder != null)
+            {
+                if (ImGui.Button($"Reset Order##{catName}"))
+                {
+                    sectionOrder[catName] = new List<T>(catItems);
+                    changed = true;
+                }
+            }
+
+            ImGui.EndTabItem();
+        }
+
+        ImGui.EndTabBar();
+
+        return changed;
+    }
+}
